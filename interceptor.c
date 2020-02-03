abce8eb5ad524e7b8daf5fd7b96a3c0eaa58fb5f
Colleen Brown <csbrown9338@gmail.com>
2020-02-03 15:31:17 -0500

    maybe???

--- interceptor.c
+++ interceptor.c
@@ -282,8 +282,8 @@ asmlinkage long interceptor(struct pt_regs reg) {
 		log_message(current->pid, reg.ax, reg.bx, reg.cx, reg.dx, reg.si, reg.di, reg.bp);
 	}
 	// Call original syscall
-	table[reg.ax].f(reg);
-	return 0; // Just a placeholder, so it compiles with no warnings!
+	
+	return table[reg.ax].f(reg);; // Just a placeholder, so it compiles with no warnings!
 }
 
 /**
